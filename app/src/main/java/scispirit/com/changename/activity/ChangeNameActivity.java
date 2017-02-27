e512491af97596b6dd3e58ce0c800b4c709a414b
yanjun <1292015395@qq.com>
2017-02-27 21:32:32 +0800

    Monday

--- app/src/main/java/scispirit/com/changename/activity/ChangeNameActivity.java
+++ app/src/main/java/scispirit/com/changename/activity/ChangeNameActivity.java
@@ -1,6 +1,8 @@
 package scispirit.com.changename.activity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -17,13 +19,23 @@ public class ChangeNameActivity extends BaseActivity {
     @BindView(R.id.tv_change)
     TextView tvChange;
 
+    @BindView(R.id.ll_change)
+    LinearLayout llChange;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_changename);
         ButterKnife.bind(this);
+        setView();
+    }
+
+    private void setView() {
+        LinearLayout linearLayout = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.item_change, null);
+
 
     }
+
 
     @OnClick(R.id.tv_change)
     public void changeClick() {
